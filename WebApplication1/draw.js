7f1d2bbffd28239eb0b98ffb40b4797b969bd5da
Shani51994 <shani_z2007@walla.com>
2019-05-29 11:32:14 +0300

    lines works

---
+++ WebApplication1/draw.js
@@ -0,0 +1,22 @@
+﻿function drawCircle(lon, lat, myCanvas) {
+    myCanvas.lineWidth = '5';
+    myCanvas.strokeStyle = "navy";
+    myCanvas.beginPath();
+    var lat = (latitude + 90) * (htmlCanvas.height / 180);
+    var lon = (lontitude + 180) * (htmlCanvas.width / 360);
+    myCanvas.arc(lon, lat, 7, 0, 2 * Math.PI);
+    myCanvas.fillStyle = "red";
+    myCanvas.fill();
+    myCanvas.stroke();
+}
+
+
+function drawPath(startLon, startLat, allLocations, myCanvas) {
+    myCanvas.beginPath();
+    myCanvas.moveTo(startLon, startLat);
+    for (var i = 0; i < allLocations.length; i++) {
+        myCanvas.lineTo(allLocations[i].x, allLocations[i].y);
+        myCanvas.strokeStyle = "red";
+        myCanvas.stroke();
+    }
+}
